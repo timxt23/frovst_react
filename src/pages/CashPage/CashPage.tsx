e6a900566c0285885a5c8beb86e25edb9800f611
timxt <timxt@MacBook-Pro-timxt.local>
2024-05-22 19:17:16 +0300

    Cors Fix

--- src/pages/CashPage/CashPage.tsx
+++ src/pages/CashPage/CashPage.tsx
@@ -164,6 +164,7 @@ export const CashPage: FC = () => {
           mb.setParams({
             text: 'Записать ' + (formData.type === 'income' ? "Приход" : "Расход"),
             textColor: '#ffffff',
+            // eslint-disable-next-line react/prop-types
             backgroundColor: formData.type === 'income' ? "#52c41a" : "#ff4d4f"
           });
           if(allFilled) {
@@ -172,6 +173,7 @@ export const CashPage: FC = () => {
             return mb.on('click', () => {fetchPostCashItem({formData})}, true);
           } else {
             mb.setParams({
+                // eslint-disable-next-line react/prop-types
                 backgroundColor: formData.type === 'income' ? "#aff08e" : "#ffb3b4"
             })
             mb.disable()
